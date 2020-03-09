bc53d7a32b6d7d794f3e793f68ea66e001932106
Bartosz Przybysz <bartosz.d.przybysz@gmail.com>
2020-03-09 11:24:35 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>macecomp</title>
+    </head>
+    <body>
+        <h1>Index of macecomp subdomain</h1>
+        <p>Using php <?php echo phpversion(); ?></p>
+    </body>
+</html>
